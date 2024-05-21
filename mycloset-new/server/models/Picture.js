35048b75d9aa71717785dde82ab8b9422c205311
Maria Julia Silva Medeiros <161225132+MariaJ001@users.noreply.github.com>
2024-05-21 18:34:31 -0300

    Delete mycloset-new directory

--- mycloset-new/server/models/Picture.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const PictureSchema = new Schema({
-  name: { type: String, required: true },
-  src: { type: String, required: true },
-});
-
-module.exports = mongoose.model("Picture", PictureSchema);
--- mycloset-new/server/routes/picture.route.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const upload = require("../config/multer");
-const PictureController = require("../controllers/pictureController");
-
-router.post("/", upload.single("file"), PictureController.create);
-router.get("/", PictureController.findAll);
-router.delete("/:id", PictureController.remove);
-
-module.exports = router;
